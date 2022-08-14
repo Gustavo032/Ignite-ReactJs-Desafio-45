692ad51c21aa41725c8800f7102daedf86a792de
Gustavo032 <gustavoramosrock@gmail.com>
2022-08-14 07:06:30 -0300

    finalizando desafio :tada:

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
 
-interface Card {
+interface Cards {
   title: string;
   description: string;
   url: string;
@@ -12,21 +12,31 @@ interface Card {
 }
 
 interface CardsProps {
-  cards: Card[];
+  cards: Cards[];
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
-
+	const { onOpen, isOpen, onClose } = useDisclosure();
   // TODO SELECTED IMAGE URL STATE
+	const [currentImageUrl, setCurrentImageUrl] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
-
+	function handleViewImage(url: string):void {
+		onOpen();
+		setCurrentImageUrl(url);
+	}
   return (
     <>
       {/* TODO CARD GRID */}
+	  <SimpleGrid columns={[1,2,3]} spacing='40px'>
+        {cards.map((card) => (
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+			<ModalViewImage isOpen={isOpen} imgUrl={currentImageUrl} onClose={onClose}/>
     </>
   );
 }
